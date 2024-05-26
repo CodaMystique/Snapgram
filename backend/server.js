0ecf8ece6107d0989c33868c1cb5e8c455022064
Anujshivhare <itachiuchihaas2001@gmail.com>
2024-05-26 18:19:06 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://snapgram-frontend.netlify.app",
+    origin: ["https://snapgram-frontend.netlify.app"],
     credentials: true,
   })
 );
