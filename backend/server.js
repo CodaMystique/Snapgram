becc64a2c334783f92ba5dccff4ef0a22ca8e45a
CodaMystique <codamystique@gmail.com>
2024-06-11 16:10:47 +0530

    vercel deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import userRoutes from "./routes/user.route.js";
